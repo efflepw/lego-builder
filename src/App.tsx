0133ca03ff5ff68c5200edd066c9de9173ac76cc
Yaroslav Sievastianov <yaroslav.sievastianov@gmail.com>
2024-06-22 12:05:48 -0700

    feat: add tip window

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { BuilderCanvas } from "./features/canvas";
 import { Interface } from "./features/interface";
@@ -6,8 +6,12 @@ import { useLocalStorage } from "./hooks";
 import { useEffect } from "react";
 import { Piece } from "./models/piece";
 import { defaultCanvasPiece, setLoadedPieces } from "./features/canvas";
+import Tips from "./components/Tips";
+import { RootState } from "./store";
 
 const App = () => {
+  const newPiece = useSelector((state: RootState) => state.interface.newPiece);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,10 +23,11 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <main className="dark bg-background h-screen">
+    <main className="dark bg-background h-screen relative">
       <Interface>
         <BuilderCanvas />
       </Interface>
+      <Tips state={newPiece == null ? "HOME" : "NEW_PIECE"} />
     </main>
   );
 };
---
+++ src/components/Tips/Tips.tsx
@@ -0,0 +1,42 @@
+const HOME_KEYS = [
+  {
+    k: "Enter",
+    text: "open add new piece window",
+  },
+];
+
+const NEW_PIECE_WINDOW_KEYS = [
+  {
+    k: "Enter",
+    text: "save new piece",
+  },
+  {
+    k: "Backspace",
+    text: "close add new piece window",
+  },
+  {
+    k: "W, A, S, D",
+    text: "move forward, left, backward, right",
+  },
+];
+
+type Props = {
+  state: "NEW_PIECE" | "HOME";
+};
+
+const Tips = ({ state }: Props) => {
+  const KEYS = state == "HOME" ? HOME_KEYS : NEW_PIECE_WINDOW_KEYS;
+
+  return (
+    <div className="bg-[#ffffff] absolute bottom-6 right-10 p-3 rounded-md opacity-70">
+      {KEYS.map((tip) => (
+        <div key={tip.k}>
+          <span className="font-bold">{tip.k}</span>
+          <span>{` - ${tip.text}`}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Tips;
